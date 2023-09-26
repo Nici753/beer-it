84001818a45974e9fe41420b7630e8ff2c4bd039
studbeerni8358 <nicolae.beer@th-rosenheim.de>
2023-09-26 16:43:43 +0200

    adding a test page

---
+++ beer-it-vue/src/views/TestPageView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="test">
+    <h1>This is a test page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .test {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
+<script setup lang="ts">
+</script>
